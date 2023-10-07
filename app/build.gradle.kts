9b0f8ad9e9d44779d67455f5e1909785164c41f8
HOANGVANSON9999 <94769991+HOANGVANSON9999@users.noreply.github.com>
2023-10-07 15:29:21 +0700

    ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("com.android.application")
     id("com.google.dagger.hilt.android")
- }
+    id("org.jetbrains.kotlin.android")
+}
 
 android {
     namespace = "com.knd.duantotnghiep.duantotnghiep"
@@ -53,4 +54,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
 }
